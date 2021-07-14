82dd2a9ec72c7cb426c08bbc1227d1ddd6ed23c9
Fabio Lima <fabiolimadesenvolvedor@gmail.com>
2021-07-14 15:14:10 -0300

    js

---
+++ Modulo D-Condições em JS/ex008.js
@@ -0,0 +1,6 @@
+var vel = 88.2
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // Condição simples!
+    console.log(`Você Ultraoassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
