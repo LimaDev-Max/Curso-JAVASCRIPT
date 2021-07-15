da398b720fe22453c162cc68ec3090868f3557ce
Fabio Lima <fabiolimadesenvolvedor@gmail.com>
2021-07-15 07:57:21 -0300

    js

--- Modulo D-Condições em JS/Aula 12-Condições(Parte2)/ex011.js
+++ Modulo D-Condições em JS/Aula 12-Condições(Parte2)/ex011.js
@@ -1,7 +1,8 @@
-var idade = 22
+var idade = 67
+console.log(`Você tem ${idade} anos.`)
 if (idade < 16) {
     console.log('Não Vota!')
-} else if (idade < 18 ) {
+} else if (idade < 18 || idade > 65) {
         console.log('Voto Opcional!')
 } else {
     console.log('Voto Obrigatorio!')
